672f6e658222ba4ba604ef38b79e4aa0e1e87f1c
Rodrigo Lima <rodrigo.lima@oracle.com>
2017-11-26 12:01:33 -0600

    automating things...

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+readonly PROGDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+echo "ZSH...."
+cp -v $HOME/.zshrc ${PROGDIR}/zshrc
+cp -vR $HOME/.oh-my-zsh/custom ${PROGDIR}/custom
+
+echo "GIT...."
+cp -v $HOME/.gitconfig ${PROGDIR}/gitconfig
+cp -v $HOME/.gitignore_global ${PROGDIR}/gitignore_global
+
+echo "VIM...."
+cp -v $HOME/.vimrc ${PROGDIR}/vimrc
+cp -vR $HOME/.vim ${PROGDIR}/vim
+
+
+# EOF
