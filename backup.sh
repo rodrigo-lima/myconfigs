452f0d131eb0ce66232067894f8d220512e350d0
Rodrigo Lima <rodrigo.lima@oracle.com>
2017-12-31 17:09:36 -0600

    update

--- backup.sh
+++ backup.sh
@@ -12,12 +12,15 @@ cp -v $HOME/.gitignore_global ${PROGDIR}/gitignore_global
 
 echo "VIM...."
 cp -v $HOME/.vimrc ${PROGDIR}/vimrc
+mkdir -p ${PROGDIR}/vim/autoload
 cp -vR $HOME/.vim/autoload/ ${PROGDIR}/vim/autoload
+mkdir -p ${PROGDIR}/vim/colors
 cp -vR $HOME/.vim/colors/ ${PROGDIR}/vim/colors
+mkdir -p ${PROGDIR}/vim/plugged
 ##  cp -vR $HOME/.vim/plugged/ ${PROGDIR}/vim/plugged
 
 echo "BREW..."
-brew list --versions > brews
+# brew list --versions > brews
 
 
 # EOF
